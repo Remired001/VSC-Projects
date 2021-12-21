1fafbfb25ec62ed888195e2a298c589d5534b062
Remi <remired001@gmail.com>
2021-12-21 12:16:38 -0800

    2nd commit added caesar cipher

---
+++ Python Labs/caesar-cypher.py
@@ -0,0 +1,122 @@
+#Store the human preproinsulin sequence in a variable called preproinsulin:
+
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
+"reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+#Store the remaining sequence elements iof human insulin in variables:
+
+lsInsulin = "malwmrllpllallalwgpdpaaa"
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+aInsulin = "giveqcctsicslyqlenycn"
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
+
+insulin = bInsulin + aInsulin
+
+# Printing "the sequence of human insulin" to console using successive print() commands:
+
+print("the sequence of human preproinsulin:")
+
+print(preproInsulin)
+
+# Printing to console using concatenated strings inside the print function (one-liner):
+  
+print("the sequence of human insulin, chain a: " + aInsulin)
+
+# Calculating the molecular weight of insulin  
+# Creating a list of the amino acid (AA) weights  
+aaWeights = {'A': 89.09, 'C': 121.16, 'D': 133.10, 'E': 147.13, 'F': 165.19,
+'G': 75.07, 'H': 155.16, 'I': 131.17, 'K': 146.19, 'L': 131.17, 'M': 149.21,
+'N': 132.12, 'P': 115.13, 'Q': 146.15, 'R': 174.20, 'S': 105.09, 'T': 119.12,
+'V': 117.15, 'W': 204.23, 'Y': 181.19}  
+# Count the number of each amino acids  
+aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A', 'C',
+'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+'V', 'W', 'Y']})  
+# Multiply the count by the weights  
+molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
+['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+'S', 'T', 'V', 'W', 'Y']}.values())  
+print("The rough molecular weight of insulin: " +
+str(molecularWeightInsulin))
+
+molecularWeightInsulinActual = 5807.63
+print("Error percentage: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
+
+# Python3.6  
+# Coding: utf-8  
+# Store the human preproinsulin sequence in a variable called preproinsulin:  
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"  
+# Store the remaining sequence elements of human insulin in variables:  
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+insulin = bInsulin + aInsulin
+
+pKR = {'y':10.07,'c':8.18,'k':10.53,'h':6.00,'r':12.48,'d':3.65,'e':4.25}
+
+insulin.count("y")
+
+float(insulin.count("y"))
+
+seqCount = ({x:float(insulin.count(x)) for x in ['y','c','k','h','r','d','e']})
+
+pH = 0
+
+netCharge = (
+    +(sum({x: ((seqCount[x]*(10**pKR[x]))/((10**pH)+(10**pKR[x]))) \
+    for x in ['k','h','r']}.values()))
+    -(sum({x: ((seqCount[x]*(10**pH))/((10**pH)+(10**pKR[x]))) \
+    for x in ['y','c','d','e']}.values())))
+    
+while (pH <= 14):
+    
+    
+        print('{0:.2f}'.format(pH), netCharge)
+
+        pH +=1
+
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet = alphabet + alphabet
+    return doubleAlphabet
+
+def getmessage():
+    stringToEncrypt = input("Please enter a message to encrypt: ")
+    return stringToEncrypt
+
+def getCipherKey():
+    shiftAmount = input ( "Please enter a key (whole number from 1-25): ")
+    return shiftAmount
+
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = ""
+    uppercaseMessage = message.upper()
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage = encryptedMessage + alphabet[newPosition]
+        else:
+            encryptedMessage = encryptedMessage + currentCharacter
+    return encryptedMessage
+
+def decryptMessage(message, cipherKey, alphabet):
+    decryptKey = -1 * int(cipherKey)
+    return encryptMessage(message, decryptKey, alphabet)
+
+def runCaesarCipherProgram():
+    myAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(f'Alphabet: {myAlphabet}')
+    myAlphabet2 = getDoubleAlphabet(myAlphabet)
+    print(f'Alphabet2: {myAlphabet2}')
+    myMessage = getmessage()
+    print(myMessage)
+    myCipherKey = getCipherKey()
+    print(myCipherKey)
+    myEncryptedMessage = encryptMessage(myMessage, myCipherKey, myAlphabet2)
+    print(f'Encrypted Message: {myEncryptedMessage}')
+    myDecryptedMessage = decryptMessage(myEncryptedMessage, myCipherKey, myAlphabet2)
+    print(f'Decypted Message: {myDecryptedMessage}')
+
+runCaesarCipherProgram()
